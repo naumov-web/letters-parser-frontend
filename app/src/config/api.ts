3ba762988d6188deec60e6fdef115f77c9b36355
Konnstantin Naumov <naumov@MacBook-Pro-Konnstantin.local>
2023-12-12 23:54:47 +0300

    LP-12: Get collection items

--- app/src/config/api.ts
+++ app/src/config/api.ts
@@ -1,3 +1,4 @@
 const BASE_API_URL = 'http://127.0.0.1:31080/api/v1';
 
 export const COLLECTIONS_URL = BASE_API_URL + '/collections';
+export const COLLECTION_ITEMS_URL = BASE_API_URL + '/collections/{collectionId}/items';
